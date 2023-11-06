a8ae4cd36406a8c6bc40487c45137edf766b493d
EhsanTgv <EhsanTgv@gmail.com>
2023-11-06 10:44:20 +0300

    Implement simple routing

--- src/main/kotlin/com/taghavi/Application.kt
+++ src/main/kotlin/com/taghavi/Application.kt
@@ -1,12 +1,30 @@
 package com.taghavi
 
-import com.taghavi.plugins.*
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
 fun Application.module() {
-    configureRouting()
+    routing {
+        get("/") {
+            call.respondText("Hello Ktor", ContentType.Text.Plain)
+        }
+
+        post("/") {
+            val post = call.receive<String>()
+            call.respondText("Received \"$post\" from the post body", ContentType.Text.Plain)
+        }
+
+        get("/users") {
+            call.respondText(userData)
+        }
+    }
 }
+
+private val userData = "{ \"users\": [\"Nate\", \"Megan\", \"John\"] }"
